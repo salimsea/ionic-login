363799a662679a35fda8ae881c638ecdfd8f5e5f
salimsea <salimajah01@gmail.com>
2022-12-07 22:53:22 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'belajar-sesi',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
